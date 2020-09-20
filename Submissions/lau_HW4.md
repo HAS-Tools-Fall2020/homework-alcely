76680ce049941adac2759074818e0eb14d7f25e0
Alcely <alcelylau@email.arizona.edu>
2020-09-20 17:34:13 -0500

    Alcely's homework 4

---
+++ Submissions/lau_HW4.md
@@ -0,0 +1,89 @@
+## Assignment 4: Forecast week 4 & Numpy Arrays
+**Name: Alcely Lau**
+
+**Date: 09/20/2020**
+
+
+
+#### Forecast summary
+
+I have a good feeling about this forecast.
+
+For this week I followed the next steps:
+
+1. Download the stream gauge observations from the USGS NWIS website, [mapper](https://maps.waterdata.usgs.gov/mapper/).
+
+2. Explore the data using **numpy arrays**. First, checking the streamflow registered in the last 3 weeks (*I am getting closer to the real weekly mean!*). Then, I analyzed the mean, quantiles (0%, 33%, 50%, 66%, and 100%) and the histograms for an specific period:
+  + Using all years in the dataset, without the year 2020, for September.
+  + Using the last 10 years, without the year 2020, for September.
+  + Using the year 2019, for September.
+  + Using the average of the previous week and the forecast week.
+
+____
+#### Assignment questions
+
+1. Include discussion of the quantitative analysis that lead to your prediction. This can include any analysis you complete but must include at least two histograms and some quantitative discussion of flow quantiles that helped you make your decision.
+
+**Last 1 week summary:**
+  + min: 53.3
+  + max: 58.2
+  + mean: 56.2
+  + std: 1.75
+
+**Statics analysis**
+
+|      years|    mean|    q=0%|   q=33%|   q=50%|   q=66%|  q=100%|
+|:---------:|:------:|:------:|:------:|:------:|:------:|:------:|
+|  All years|  180.08|    48.6|   95.98|  115.00|  141.00| 5590.00|
+|    >= 2009|  126.28|    48.6|   91.74|  105.50|  130.94|  776.00|
+|       2019|   66.49|    48.6|   52.76|   58.85|   **61.31**|  121.00|
+
+**Histograms**
+
+![alt text](https://github.com/HAS-Tools-Fall2020/homework-alcely/blob/master/assignment_4/Histogram_historical.png "Histogram_historical")
+
+![alt text](https://github.com/HAS-Tools-Fall2020/homework-alcely/blob/master/assignment_4/Histogram_2019_09.png "Histogram_2019_09")
+
+Observing at the histogram of September 2019 (*the second histogram*), the most frequent streamflows are in the range between ~35 to ~65 cfs. Hence, let think that the intensity of the 2020 drought will decrease and my week 1 forecast will be the 66% quantile of the year 2019.
+
+-----
+2. Describe the variable flow_data:
+  - What is it?
+  - What type of values it is composed of?
+  - What is are its dimensions, and total size?
+
+
+   **Response:** The variable `flow_data` is a **numpy.ndarray** . It is composed of **floats** (*in a numpy array must be the same data type*). It has **2 dimensions** and its total size is: **(11585, 4)** , where (rows, columns).
+
+-----
+3. How many times was the daily flow greater than your prediction in the month of September (express your answer in terms of the total number of times and as a percentage)?
+**Response:**
+
+| September | Forecast value (cfs) | Number of times | Percentage |
+|:---------:|:--------------------:| :--------------:|:----------:|
+|    week 1 |                   65 |             876 |    92.31 % |
+|    week 2 |                   67 |             870 |    91.68 % |
+|    week 3 |                54.05 |             921 |    97.05 % |
+|   average |                62.02 |             883 |    93.04 % |
+
+-----
+
+4. How would your answer to the previous question change if you considered only daily flows in or before 2000? Same question for the flows in or after the year 2010? (again report total number of times and percentage)
+**Response:**
+
+| Years | September | Forecast value (cfs) | Number of times | Percentage |
+|:-----:|:---------:|:--------------------:| :--------------:|:----------:|
+|<= 2000|    week 1 |                   65 |             359 |    37.83 % |
+|<= 2000|    week 2 |                   67 |             356 |    37.51 % |
+|<= 2000|    week 3 |                54.05 |             360 |    37.93 % |
+|<= 2000|   average |                62.02 |             360 |    37.93 % |
+|>= 2010|    week 1 |                   65 |             276 |    29.08 % |
+|>= 2010|    week 2 |                   67 |             273 |    28.77 % |
+|>= 2010|    week 3 |                54.05 |             302 |    31.82 % |
+|>= 2010|   average |                62.02 |             279 |    29.40 % |
+
+-----
+
+5. How does the daily flow generally change from the first half of September to the second?
+
+   **Response:** The average daily flows on the first half of September is: **178.07** cfs , in contrast, on the second half of September is: **168.88** cfs.
---
+++ Submissions/lau_HW4.py
@@ -0,0 +1,265 @@
+# Starter code for Homework 4
+
+# %%
+# Import the modules we will use
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# ** MODIFY **
+# Set the file name and path to where you have stored the data
+filename = 'streamflow_week4.txt'
+filepath = os.path.join('C:/Users/alcel/Documents/GitHub/2020FA_CodingSkills/homework-alcely/data', filename)
+print(os.getcwd())
+print(filepath)
+
+# %%
+# DON'T change this part -- this creates the lists you 
+# should use for the rest of the assignment
+# no need to worry about how this is being done now we will cover
+# this in later sections. 
+
+#Read the data into a pandas dataframe
+data=pd.read_table(filepath, sep = '\t', skiprows=30,
+        names=['agency_cd', 'site_no', 'datetime', 'flow', 'code']
+        )
+
+# Expand the dates to year month day
+data[["year", "month", "day"]] =data["datetime"].str.split("-", expand=True)
+data['year'] = data['year'].astype(int)
+data['month'] = data['month'].astype(int)
+data['day'] = data['day'].astype(int)
+
+# Make a numpy array of this data
+flow_data = data[['year', 'month','day', 'flow']].to_numpy()
+
+# Getting rid of the pandas dataframe since we wont be using it this week
+del(data)
+
+# %%
+# Assignment 4 question2:
+
+print("The variable `flow_data` is", type(flow_data), \
+        ". It is composed of floats (in a numpy array must be the same data type).", \
+                "It has", flow_data.ndim, "dimensions and its total size is:", \
+                flow_data.shape, ", where (rows, columns).")
+
+# %% Checking that the data have the same type in each column.
+type(flow_data[0,0])
+type(flow_data[0,1])
+type(flow_data[0,2])
+type(flow_data[0,3])
+
+# %%
+# Assignment 4 question3:
+# Number of times my forecast is greater than the daily flow in September
+weeks_text = ["week 1", "week 2", "week 3", "average"]
+forecast = [65, 67, 54.05, 62.02] # Insert forecast
+
+print("| September | Forecast value (cfs) | Number of times | Percentage |")
+
+# Count the number of values with daily flow > forecast and month ==9
+for i in range(len(forecast)):
+        times_count = np.sum((flow_data[:,3] > forecast[i]) & (flow_data[:,1]==9))
+        sept_days = np.sum(flow_data[:,1]==9)
+
+        # In percetange:
+        per = 100 * (times_count / sept_days)
+
+        print("|", weeks_text[i], "|", forecast[i], "|", \
+                times_count, "|", per, "% |")
+
+# %%
+# Assignment 4 question4: 
+print("| Years  | September | Forecast value (cfs) | Number of times | Percentage |")
+
+# Count the number of values with daily flow > forecast and month ==9 
+# and years are <=2000
+for i in range(len(forecast)):
+        times_count = np.sum((flow_data[:,3] > forecast[i]) & (flow_data[:,1]==9) \
+                & (flow_data[:,0] <= 2000))
+        sept_days = np.sum(flow_data[:,1]==9)
+
+        # In percetange:
+        per = 100 * (times_count / sept_days)
+        
+        print("|<= 2000|", weeks_text[i], "|", forecast[i], "|", \
+                times_count, "|", per, "% |")
+
+# Count the number of values with daily flow > forecast and month ==9 
+# and years are >=2010
+for i in range(len(forecast)):
+        times_count = np.sum((flow_data[:,3] > forecast[i]) & (flow_data[:,1]==9) \
+                & (flow_data[:,0] >=2010))
+        sept_days = np.sum(flow_data[:,1]==9)
+
+        # In percetange:
+        per = 100 * (times_count / sept_days)
+
+        print("|>= 2010|", weeks_text[i], "|", forecast[i], "|", \
+                times_count, "|", per, "% |")
+
+# %%
+# Assignment 4 question5:
+
+half1 = np.mean(flow_data[(flow_data[:,1]==9) & (flow_data[:,2] <= 15), 3])
+half2 = np.mean(flow_data[(flow_data[:,1]==9) & (flow_data[:,2] > 15), 3])
+
+print("The average daily flows on the first half of September is:", half1, \
+        "cfs , in contrast, on the second half of September is:", half2, "cfs.")
+
+# %% 
+# Here start my HW3 code.
+# Step1: Grabbing out the data from the last 3 weeks.
+
+flow_last3week = np.array(flow_data[-21:,:])
+flow_last2week = np.array(flow_data[-14:,:])
+flow_last1week = np.array(flow_data[-7:,:])
+
+#%%
+# Step2: Calculating some basic properites from the data of the last 3 weeks
+
+flow_last3week_min = np.min(flow_last3week[:,3])
+flow_last3week_max = np.max(flow_last3week[:,3])
+flow_last3week_mean = np.mean(flow_last3week[:,3])
+flow_last3week_std = np.std(flow_last3week[:,3])
+
+flow_last2week_min = np.min(flow_last2week[:,3])
+flow_last2week_max = np.max(flow_last2week[:,3])
+flow_last2week_mean = np.mean(flow_last2week[:,3])
+flow_last2week_std = np.std(flow_last2week[:,3])
+
+flow_last1week_min = np.min(flow_last1week[:,3])
+flow_last1week_max = np.max(flow_last1week[:,3])
+flow_last1week_mean = np.mean(flow_last1week[:,3])
+flow_last1week_std = np.std(flow_last1week[:,3])
+
+# %%
+# Summary of the last 3 weeks streamflows
+print("The flows from the last 3 weeks")
+print(flow_last3week)
+print("")
+print("The flows from the last 2 weeks")
+print(flow_last2week)
+print("")
+print("The flows from the last week")
+print(flow_last1week)
+print("")
+
+print("Last 3 weeks summary:")
+print("min:", flow_last3week_min)
+print("max:", flow_last3week_max)
+print("mean:", flow_last3week_mean)
+print("std:", flow_last3week_std)
+print("")
+
+print("Last 2 weeks summary:")
+print("min:", flow_last2week_min)
+print("max:", flow_last2week_max)
+print("mean:", flow_last2week_mean)
+print("std:", flow_last2week_std)
+print("")
+
+print("Last 1 week summary:")
+print("min:", flow_last1week_min)
+print("max:", flow_last1week_max)
+print("mean:", flow_last1week_mean)
+print("std:", flow_last1week_std)
+print("")
+
+# %%
+#Step4: historical data analysis.
+# modify the following variables:
+m = 9           #Insert month
+fd = 13         #Insert first day of the week
+ed= 26          #Insert end day of the week
+y = 2019        #Insert year
+
+# historical mean, without the year 2020.
+flow_mean1 = np.mean(flow_data[(flow_data[:,0] != 2020) & \
+        (flow_data[:,1] == m) & (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3])
+
+# mean since the year 2009 (the last 10 years), without the year 2020.
+flow_mean2 = np.mean(flow_data[(flow_data[:,0] != 2020) & (flow_data[:,0] >= 2009) &\
+         (flow_data[:,1] == m) & (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3])
+
+# mean for a specific year.
+flow_mean3 = np.mean(flow_data[(flow_data[:,0] == y) & (flow_data[:,1] == m) &\
+         (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3])
+
+print("Using all the historical data, without the year 2020, for the month =", m, \
+        ", during the week between days", fd, "and", ed, "the mean flow is =", flow_mean1)
+print("")
+print("Using the last 10 years data, without the year 2020, for the month =", m, \
+        ", during the week between days", fd, "and", ed, "the mean flow is =", flow_mean2)
+print("")
+print("In the year", y, "for the month =", m, ", during the week between days", \
+        fd, "and", ed, "the mean flow is =", flow_mean3)
+print("")
+
+# %%
+# Quantiles
+# historical quantiles, without the year 2020.
+flow_quants1 = np.quantile(flow_data[(flow_data[:,0] != 2020) & \
+        (flow_data[:,1] == m) & (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3],\
+                 q=[0,0.33,0.5,0.66,1.0])
+
+# Quantiles since the year 2009 (the last 10 years), without the year 2020.
+flow_quants2 = np.quantile(flow_data[(flow_data[:,0] != 2020) & (flow_data[:,0] >= 2009) &\
+         (flow_data[:,1] == m) & (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3],\
+                  q=[0,0.33,0.5,0.66,1.0])
+
+# Quantiles for a specific year.
+flow_quants3 = np.quantile(flow_data[(flow_data[:,0] == y) & (flow_data[:,1] == m) &\
+         (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3], q=[0,0.33,0.5,0.66,1.0])
+
+print("All years, month =", m, "week between days:", fd, "and", ed)
+print("min, 33%, median, 66%, max")
+print(flow_quants1)
+print("")
+print("Last 10 years, month =", m, "week between days:", fd, "and", ed)
+print("min, 33%, median, 66%, max")
+print(flow_quants2)
+print("")
+print("year =", y, "month =", m, "week between days:", fd, "and", ed)
+print("min, 33%, median, 66%, max")
+print(flow_quants3)
+print("")
+
+# %%
+#using all years, without year 2020, and a specific month
+flow_slice1 = np.array(flow_data[(flow_data[:,0] != 2020) & (flow_data[:,1] == m) ,3])
+
+mybins1 = np.linspace(0, 200, num=10) 
+#Plotting the histogram
+plt.hist(flow_slice1, bins = mybins1)
+plt.title('Historical Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+
+# %%
+#using an specific year and month
+flow_slice2 = np.array(flow_data[(flow_data[:,0] == y) & (flow_data[:,1] == m) ,3])
+
+mybins2 = np.linspace(0, np.max(flow_slice2), num=5) 
+#Plotting the histogram
+plt.hist(flow_slice2, bins = mybins2)
+plt.title('Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+# %%
+#using all years, without year 2020, and a specific month
+flow_slice3 = np.array(flow_data[(flow_data[:,0] != 2020) & (flow_data[:,1] == m) & (flow_data[:,2] >= fd) & (flow_data[:,2] <= ed),3])
+
+mybins3 = np.linspace(0, 200, num=10) 
+#Plotting the histogram
+plt.hist(flow_slice3, bins = mybins3)
+plt.title('Historical Streamflow')
+plt.xlabel('Flow [cfs]')
+plt.ylabel('Count')
+
+# %%
